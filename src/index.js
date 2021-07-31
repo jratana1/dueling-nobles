270c787162e35373c546645196e79fcfb36a5291
John Ratana <jratana1@gmail.com>
2021-07-31 11:21:57 -0400

    setting up redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer.js';
+
+const store = createStore(rootReducer,
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
